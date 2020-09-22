691d57142185115b46f0a91eeb709bd3118b32df
mdubchak <dubchmar@gmail.com>
2020-09-22 17:10:26 +0300

    Updates

---
+++ precache-manifest.f3839f1d8402d5f119f1801f9cc3e74f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04850de9d980ae493172e0911441febb",
+    "url": "/Test-react/index.html"
+  },
+  {
+    "revision": "36c23876ef8a9919877c",
+    "url": "/Test-react/static/css/main.c8709d5e.chunk.css"
+  },
+  {
+    "revision": "7160d6e9ff3dbf1c1036",
+    "url": "/Test-react/static/js/2.4464bc9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Test-react/static/js/2.4464bc9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c23876ef8a9919877c",
+    "url": "/Test-react/static/js/main.a6db1278.chunk.js"
+  },
+  {
+    "revision": "0e1523f84640b911b88d",
+    "url": "/Test-react/static/js/runtime-main.437b7bd3.js"
+  }
+]);
